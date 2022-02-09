14be7a8dc63a451a7209478b1a28abe125440880
KAMA <kimokamel223@gmail.com>
2022-02-09 16:03:10 +0100

    Add Header and Map Pages

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 function Header() {
+  const classes = useStyles();
   return (
-    <div>Header</div>
-  )
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Companion
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Travel Companion
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search ..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
--- src/component/Map/Map.jsx
+++ src/component/Map/Map.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons";
+import GoogleMapReact from "google-map-react";
+import React from "react";
+import useStyles from "./styles";
 
 function Map() {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>Map</div>
-  )
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCu-xEUxGhsKMZE-7Zya46V2lNVl50OrbQ" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        onChange={""}
+        options={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
 export default Map;
