2c50a9b9e7e5247d06a58da28f78a93799902d9b
KAMA <kimokamel223@gmail.com>
2022-02-09 15:15:00 +0100

    Add Grid Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./component/Header/Header";
+import List from "./component/List/List";
+import Map from "./component/Map/Map";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
