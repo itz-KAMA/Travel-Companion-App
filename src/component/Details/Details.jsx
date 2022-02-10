13a92d23a42cfc45b182e8c3b1d289d31550f9e5
KAMA <kimokamel223@gmail.com>
2022-02-10 12:13:02 +0100

    Add Details To List

--- src/component/Details/Details.jsx
+++ src/component/Details/Details.jsx
@@ -1,11 +1,102 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import { Rating } from "@material-ui/lab";
+import useStyles from "./styles";
 
 function Details({ place }) {
-  console.log(place);
+  const classes = useStyles();
   return (
-    <div>
-      <h1>{place.name}</h1>
-    </div>
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://www.google.tn/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F48%2Fd7%2F28%2F48d7280a30c2ea409c2fe596fb471f19.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F392939136221244652%2F&tbnid=PPDo-VXgSE3IpM&vet=12ahUKEwits46b-_T1AhWMxosKHVtwB1IQMygEegUIARCvAQ..i&docid=EZiCi94uvh_yIM&w=800&h=600&q=restaurant%20not%20found&ved=2ahUKEwits46b-_T1AhWMxosKHVtwB1IQMygEegUIARCvAQ"
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {place.name}
+        </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Price</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.price_level}
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Ranking</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.ranking}
+          </Typography>
+        </Box>
+        {place?.awards?.map((award) => (
+          <Box
+            my={1}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <img src={award.images.small} alt={award.display_name} />
+            <Typography variant="subtitle2" color="textSecondary">
+              {award.display_name}
+            </Typography>
+          </Box>
+        ))}
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant="subtitle2"
+            color="textSecondary"
+            className={classes.subtitle}
+          >
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant="subtitle2"
+            color="textSecondary"
+            className={classes.spacing}
+          >
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => window.open(place.web_url, "_blank")}
+          >
+            Trip Advisor
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => window.open(place.website, "_blank")}
+          >
+            Website
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
   );
 }
 
